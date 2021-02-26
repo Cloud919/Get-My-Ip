f93e67366cefca10f25d845d72694f1b4618f6ce
SeYoung <mswn0919@kakao.com>
2021-02-26 23:12:27 +0900

    Header and Footer Updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Footer.css';
 
 export default class Footer extends React.Component {
     render(){
         return(
-            <div>This is Footer</div>
+            <div className="footer">
+                <div className="footer_inner">
+                    <div className="footer_icon">
+                        <a href="https://github.com/Cloud919" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+                        <a href="mailto:mswn0919@kakao.com"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,13 @@ import './Header.css';
 export default class Header extends React.Component {
     render(){
         return(
-            <div>This is Header</div>
+            <div className="header">
+                <a href="./" className="logo">Get My IP</a>
+                <div className="header-right">
+                    <a href="./">Home</a>
+                    <a href="https://github.com/Cloud919" target="_blank" rel="noreferrer">GitHub</a>
+                </div>
+            </div>
         );
     }
 }
